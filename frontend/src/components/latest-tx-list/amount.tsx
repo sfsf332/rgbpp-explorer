995160c4541f36d203bc3862276195d0e6dfd575
Jack <6464072@qq.com>
2025-04-22 12:16:20 +0800

    fix:lint error

--- frontend/src/components/latest-tx-list/amount.tsx
+++ frontend/src/components/latest-tx-list/amount.tsx
@@ -3,7 +3,7 @@
 import { Trans } from '@lingui/macro'
 
 import { Text } from '@/components/ui'
-import { formatNumber } from '@/lib/string/format-number'
+// import { formatNumber } from '@/lib/string/format-number'
 import { CkbTransaction } from '@/types/graphql'
 
 export function Amount({ transaction }: { transaction?: CkbTransaction | null }) {
